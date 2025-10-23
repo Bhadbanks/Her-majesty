860aae35b33432bfc8591a58e754c710726c35f5
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-23 06:21:26 -0700

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer className="absolute bottom-6 right-10 z-20 text-white text-2xl text-glow opacity-80">
+      — From Ayomide 🌹💫
+    </footer>
+  );
+}
