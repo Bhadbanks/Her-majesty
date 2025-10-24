a7ccdd32cc14387e8a5f0124d08b9d7e2f81a0c0
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-24 04:39:49 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppRoutes />
     </BrowserRouter>
   );
 }
-
-export default App;
