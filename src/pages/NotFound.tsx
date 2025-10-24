4b1eee94e2a4168cd9ccff6af15422f8f93a3a67
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-24 02:36:16 -0700

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center text-center">
+      <div>
+        <h1 className="text-4xl font-bold mb-2">404 — Page Not Found</h1>
+        <p className="text-gray-500">If this showsthe there'san error🤕</p>
+      </div>
+    </div>
+  )
+}
