477f2dd154f2ef2a83420e0182140fdbb2c6a5d6
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-24 05:09:29 -0700

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -3,7 +3,7 @@ import Home from "../pages/home/page";
 
 const routes: RouteObject[] = [
   { path: "/", element: <Home /> },
-  { path: "*", element: <Home /> } // fallback to home to avoid blank routes
+  { path: "*", element: <Home /> }
 ];
 
 export default routes;
