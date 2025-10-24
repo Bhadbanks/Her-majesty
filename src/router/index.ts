1423c28c19af54cf2e7e3347e14ad332cbda1e7c
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-24 05:10:12 -0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,6 @@
-import { useNavigate, useRoutes } from "react-router-dom";
-import { useEffect } from "react";
+import { useRoutes } from "react-router-dom";
 import routes from "./config";
 
-let navigateResolver: any;
-export const navigatePromise = new Promise((resolve) => {
-  navigateResolver = resolve;
-});
-
 export function AppRoutes() {
-  const element = useRoutes(routes);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!(window as any).REACT_APP_NAVIGATE) {
-      (window as any).REACT_APP_NAVIGATE = navigate;
-      navigateResolver(navigate);
-    }
-  }, [navigate]);
-
-  return element;
+  return useRoutes(routes);
 }
