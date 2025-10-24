7850844d986d87a17a2b9e2690a8adc0a15b75b7
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-24 02:39:02 -0700

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,5 +1,5 @@
 import type { RouteObject } from "react-router-dom";
-import NotFound from "../pages/NotFound";
+//import NotFound from "../pages/NotFound";
 import Home from "../pages/home/page";
 const routes: RouteObject[] = [
   {
